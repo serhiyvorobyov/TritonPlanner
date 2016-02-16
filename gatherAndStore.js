3474acc11af651b78cd8366deecffd3b7bafc692
Serhiy Vorobyov <serhiy.vorobyov@gmail.com>
2016-02-16 15:17:48 -0800

    courses now parse into json file properly. Bugs out every once in a while, may need to fix down the road

--- gatherAndStore.js
+++ gatherAndStore.js
@@ -2,45 +2,118 @@
     // Modules
     var request = require('request');
     var cheerio = require('cheerio');
-    var YQL = require('yql');
-
+    var fs = require('fs');
 
     // Data
     var URLsArray = require('./data/course-catalog-URLs.json').catalog;
     var courseData = require('./data/courses.json');
 
     // Variables
+    var department;
+    var url;
     var $;
 
-    //console.log(URLsArray);
-    request('http://www.ucsd.edu/catalog/courses/CSE.html', function (err, res, body) {
-        if (!err && res.statusCode == 200) {
-            $ = cheerio.load(body);
+    var i;
+    var length = URLsArray.length;
+    for( i=0; i<length; i++ ) {
+        //console.log(URLsArray);
+        url = URLsArray[i].URL;
 
-            // Grab all of the course titles
-            $('.course-name').each(function(i, elem) {
-                var courseName = $(this).text();
-                var courseNumbers = courseName.split('.')[0].split("/"); // In the case: COURSE/COURSE
-                var courseTitle = courseName.split('.')[1];
+        // Be careful. This is an asynchronous call.
+        request(url, function (err, res, body) {
+            if (!err && res.statusCode == 200) {
+                $ = cheerio.load(body);
+                var deptClasses = [];
+                var json;
+                var courseName;
+                var courseNumbers;
+                var courseUnits;
                 var courseParts;
-                var courseID;
 
-                /* For all detected courses (if multiple) remove special characters,
-                   make department lowercase, and replace current array index with new class name.
-                 */
-                courseNumbers.forEach( function(elem, i, arr) {
-                    courseParts = elem.replace(/[^\w\s]/gi, '').split(/\s+/);
-                    //console.log(courseParts);
-                    arr[i] = courseParts[0].toLowerCase() + courseParts[1];
-                })
+                // The next element after the heading (upper/lower div) is an <a> element to support anchors.
+                // I am grabbing the very first one to determine which department we are in.
+                department = $('.course-subhead-1').next().attr('name').replace(/[^a-zA-Z]/, '');
 
-                // Merge courses together if there are multiple
-                courseID = courseNumbers.join('/');
-                console.log(courseID, courseTitle, '\n');
+                // Grab all of the course titles
+                $('.course-name').each(function(i, elem) {
+                    courseName = $(this).text();
+                    courseNumbers = courseName.split('.')[0].split("/"); // In the case: COURSE/COURSE
+                    courseUnits = courseName.split('.')[1].indexOf("(");
+                    json = {
+                        id: "",
+                        name: "",
+                        nameShort: "",
+                        units: "",
+                        description: "",
+                        evalsURL: "http://cape.ucsd.edu/responses/Results.aspx?courseNumber=",
+                        addURL: "/",
+                        prereqs: []
+                    };
 
+                    // Course number may not have department in front it so must insert one.
+                    var classId =
+                        courseNumbers[0].split(' ').length > 1 ?
+                            courseNumbers[0].split(' ').join('+') : department.toUpperCase() + "+" + courseNumbers[0];
+                    json.nameShort =
+                        courseNumbers[0].split(' ').length > 1 ?
+                            courseNumbers[0] : department.toUpperCase() + " " + courseNumbers[0];
+
+                    json.evalsURL = json.evalsURL + classId;
+
+
+                    //console.log(json.evalsURL);
+                    /* For all detected courses (if multiple) remove special characters,
+                       make department lowercase, and replace current array index with new class name.
+                     */
+                    courseNumbers.forEach( function(elem, index, arr) {
+                        courseParts = elem.replace(/[^\w\s]/gi, '').split(/\s+/);
+                        if( courseParts.length > 1) {
+                            arr[index] = courseParts[0].toLowerCase() + courseParts[1];
+                        } else {
+                            arr[index] = department + courseParts[0];
+                        }
+
+                    });
+
+                    // Merge courses together if there are multiple
+                    json.id = courseNumbers[1] ? courseNumbers.join('/'): courseNumbers[0];
+                    json.name = courseName.split('.')[1].split(" (")[0].substr(1); // Grab title up to the units
+                    json.units = courseName.split('.')[1].substr(courseUnits);
+
+                    deptClasses.push(json);
+                });
+
+                $('.course-descriptions').each(function(index, elem) {
+                    var courseDescription = $(this).text().split(' Prerequisites: ');
+                    if( index < deptClasses.length) {
+                        deptClasses[index].description = courseDescription[0];
+                        deptClasses[index].prereqs.push(courseDescription[1] ? courseDescription[1] : "none");
+                    }
+
+                });
+
+                var lengthDeptArr = deptClasses.length;
+                var j;
+
+                for( j=0; j<lengthDeptArr; j++ ) {
+                    courseData[deptClasses[j].id] = deptClasses[j];
+                }
+
+                console.log(deptClasses);
+
+            } else {
+                console.log("Issue with request...");
+            }
+
+            fs.writeFile('./data/courses.json', JSON.stringify(courseData, null, 4), function(err) {
+                if(!err) {
+                    console.log("You've successfully updated courses.json with the most current classes and descriptions!");
+                } else {
+                    console.log(err);
+                }
             });
+        });
+    }
 
-            //console.log($('p.course-descriptions').nextAll().html());
-        }
-    });
+
 })();
